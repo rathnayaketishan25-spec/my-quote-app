96db4a0af223001e38250c3907a38c77bda4d652
rathnayaketishan25-spec <rathnayaketishan25@gmail.com>
2026-05-11 00:49:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const quoteBtn = document.getElementById('new-quote');
 
-// අන්තර්ජාලයෙන් Quotes ගන්නා Function එක
 async function getQuote() {
-    // Quote එක එනකම් පොඩි Loading පණිවිඩයක් පෙන්වමු
-    quoteText.innerText = "Loading...";
+    quoteText.innerText = "ගෙන්නන ගමන්...";
     authorText.innerText = "";
-
+    
     try {
-        const response = await fetch('https://api.quotable.io/random');
+        // අපි වෙනත් API එකක් පාවිච්චි කරමු
+        const response = await fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random'));
         const data = await response.json();
+        const quoteData = JSON.parse(data.contents);
 
-        // API එකෙන් ලැබෙන දත්ත ඇප් එකට දාමු 
-        quoteText.innerText = data.content;
-        authorText.innerText = "- " + data.author;
+        quoteText.innerText = quoteData[0].q;
+        authorText.innerText = "- " + quoteData[0].a;
     } catch (error) {
-        // අන්තර්ජාලය නැත්නම් හෝ මොකක් හරි වැරදුනොත් කලින් වගේ අපේම එකක් පෙන්වමු
-        quoteText.innerText = "සාර්ථකත්වය යනු වැටෙන හැම වතාවකම නැගිටීමයි.";
-        authorText.innerText = "- වින්ස්ටන් චර්චිල්";
-        console.log("Error logic: ", error);
+        quoteText.innerText = "අන්තර්ජාලයේ පොඩි අවුලක්. ආයෙත් ට්‍රයි කරන්න!";
+        authorText.innerText = "";
     }
 }
 
-// බටන් එක එබුවාම getQuote function එක වැඩ කරන්න
 quoteBtn.addEventListener('click', getQuote);
-
-// පේජ් එක මුලින්ම Load වෙද්දී Quote එකක් පෙන්වන්න
 getQuote();
