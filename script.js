f2564934146669c6741f6fa3343e2bb6395fb99e
rathnayaketishan25-spec <rathnayaketishan25@gmail.com>
2026-05-11 01:40:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const quoteBtn = document.getElementById('new-quote');
 
 // අපිම හදාගත්ත Quotes ලිස්ට් එක. මේකට ඔයාට ඕන තරම් පස්සේ එකතු කරන්න පුළුවන්.
 const myQuotes = [
-    { q: "The only way to do great work is to love what you do.", a: "Steve Jobs" },
-    { q: "Believe you can and you're halfway there.", a: "Theodore Roosevelt" },
-    { q: "Success is not final, failure is not fatal: it is the courage to continue that counts.", a: "Winston Churchill" },
-    { q: "It always seems impossible until it's done.", a: "Nelson Mandela" },
-    { q: "Your time is limited, so don't waste it living someone else's life.", a: "Steve Jobs" },
-    { q: "Hardships often prepare ordinary people for an extraordinary destiny.", a: "C.S. Lewis" }
+    { q: "The only way to do great work is to love what you do.", a: "Steve Jobs" , img: "https://...steve-jobs.jpg"},
+    { q: "Believe you can and you're halfway there.", a: "Theodore Roosevelt" , img : "https://...theodore-roosevelt.jpg " },
+    { q: "Success is not final, failure is not fatal: it is the courage to continue that counts.", a: "Winston Churchill" , img: "https://...winston-churchill" },
+    { q: "It always seems impossible until it's done.", a: "Nelson Mandela" , img: "https://...nelson-mandela" },
+    { q: "Your time is limited, so don't waste it living someone else's life.", a: "Steve Jobs" , img: "https://...steve-jobs.jpg" },
+    { q: "Hardships often prepare ordinary people for an extraordinary destiny.", a: "C.S. Lewis" , img: "https://...c.s.-lewis" }
 ];
 
 function getQuote() {
