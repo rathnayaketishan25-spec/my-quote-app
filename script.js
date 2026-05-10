44207f5de1422c469540963906c0f73739fc442a
rathnayaketishan25-spec <rathnayaketishan25@gmail.com>
2026-05-11 01:54:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ const quoteBtn = document.getElementById('new-quote');
 
 // අපිම හදාගත්ත Quotes ලිස්ට් එක. මේකට ඔයාට ඕන තරම් පස්සේ එකතු කරන්න පුළුවන්.
 const myQuotes = [
-    { q: "The only way to do great work is to love what you do.", a: "Steve Jobs" , img: "https://...steve-jobs.jpg"},
-    { q: "Believe you can and you're halfway there.", a: "Theodore Roosevelt" , img : "https://...theodore-roosevelt.jpg " },
-    { q: "Success is not final, failure is not fatal: it is the courage to continue that counts.", a: "Winston Churchill" , img: "https://...winston-churchill" },
-    { q: "It always seems impossible until it's done.", a: "Nelson Mandela" , img: "https://...nelson-mandela" },
-    { q: "Your time is limited, so don't waste it living someone else's life.", a: "Steve Jobs" , img: "https://...steve-jobs.jpg" },
-    { q: "Hardships often prepare ordinary people for an extraordinary destiny.", a: "C.S. Lewis" , img: "https://...c.s.-lewis" }
+    { q: "The only way to do great work is to love what you do.", a: "Steve Jobs", img: "https://images.services.disruptive-technologies.com/1I1S9Y7fS8e478581e69502.jpg" },
+    { q: "Believe you can and you're halfway there.", a: "Theodore Roosevelt", img: "https://upload.wikimedia.org/wikipedia/commons/1/1c/President_Thodore_Roosevelt_2012_retouched_crop.jpg" },
+    { q: "Success is not final, failure is not fatal: it is the courage to continue that counts.", a: "Winston Churchill", img: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Sir_Winston_Churchill_-_1941_Portrait.jpg" },
+    { q: "It always seems impossible until it's done.", a: "Nelson Mandela", img: "https://upload.wikimedia.org/wikipedia/commons/0/02/Nelson_Mandela_1994.jpg" },
+    { q: "Your time is limited, so don't waste it living someone else's life.", a: "Steve Jobs", img: "https://images.services.disruptive-technologies.com/1I1S9Y7fS8e478581e69502.jpg" },
+    { q: "Hardships often prepare ordinary people for an extraordinary destiny.", a: "C.S. Lewis", img: "https://upload.wikimedia.org/wikipedia/en/1/1e/C._S._Lewis_cropped.jpg" }
 ];
 
 function getQuote() {
     // ලිස්ට් එකෙන් අහඹු ලෙස එකක් තෝරාගන්නා හැටි
     const randomIndex = Math.floor(Math.random() * myQuotes.length);
     const selectedQuote = myQuotes[randomIndex];
-    
+
     quoteText.innerText = selectedQuote.q;
     authorText.innerText = "- " + selectedQuote.a;
-     document.getElementById('author-image').src = selectedQuote.img;
+    
+    // පින්තූරය පෙන්වන පේළිය
+    document.getElementById('author-image').src = selectedQuote.img;
 }
 
 // බටන් එක එබුවාම වැඩ කරන්න
